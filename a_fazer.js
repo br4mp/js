36ba53d7b22d5cab799c745914d820afc2abfe15
Brenno <brennoudi@gmail.com>
2019-06-24 22:20:59 -0300

    App A Fazer

--- a_fazer.js
+++ a_fazer.js
@@ -1,1 +1,23 @@
-alert('Workou!');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input')
+var inputElement = document.querySelector('#app button')
+
+var a_fazer = [
+    'Fazer Café',
+    'Cozinhar',
+    'Emagrecer',
+    'Get Rich',
+];
+
+function renderAfazer() {
+    for (item of a_fazer) {
+        var itemElement = document.createElement('li');
+        var itemText = document.createTextNode(item);
+
+        itemElement.appendChild(itemText);
+        listElement.appendChild(itemElement);
+    
+    }
+}
+
+renderAfazer();
